e117c2802d33afd763a97bfdd40c0002e4f8ff7c
suryaprasath <suryaprasathram@gmail.com>
2022-10-12 10:05:41 +0530

    midsem

--- asg/utils.py
+++ asg/utils.py
@@ -1,6 +1,4 @@
 from math import sqrt
-import matplotlib.pyplot as plt
-
 
 def line_btw(c1, c2):
     """Computes the line between two points
@@ -51,5 +49,55 @@ def dist_btw_polygon(c, p):
 
 
 def tangent_to_polygon(p):
+    """Computes the tangent of a point to another polygon
+    
+        argument: p (tuple of (float, float)) polygon
+        return: tangents (list of int) 4 possibles tangent
+    """
+    
+    m0 = {}
+    m_vol0 = 9999
+    dist_max = 0
+    for vertice in p:
+        d = dist_btw(p[0], vertice)
 
-    pass
+        if d > dist_max:
+            dist_max = d
+            vertice_max = vertice
+            index_max = p.index(vertice)
+
+    for vertice in p:
+        # tangent = 0 <-> m = +infinite
+        m0[p.index(vertice)] = dist_btw(p[-1], vertice) / dist_btw(p[0], vertice)
+        vol0 = (m0[p.index(vertice)] * dist_btw(p[0], vertice)**2) / 2
+        if vol0 < m_vol0:
+            m_vol0 = vol0
+            n = p.index(vertice)
+
+    # return the two tangents seperated by a \
+    tangents = [index_max, n]
+    n = (n + 1) % len(p)
+    tangents.append(n)
+
+    n = (n + 1) % len(p)
+    if n == (index_max + 1) % len(p):
+        pass
+    else:
+        tangents.append(n)
+
+    return tangents
+
+
+def polygon_intersection(list_polygons):
+    """compute the intersection points from multiple polygons
+    
+    argument: list_polygons (tuple of tuple of (float, float)) mulitiple polygons
+    return: intersection ( tuple of float) intersection points of the polygon
+    """
+
+    ordered_polygon = []
+    for polygon in list_polygons:
+        tangents = tangent_to_polygon(polygon)
+        ordered_polygon.append(list(polygon[i] for i in tangents))
+
+    return ordered_polygon
